1f9a15dea31596a0410e1fd9280bf19fb017d72c
lloyd9 <jcachondo@spectrumone.co>
2019-08-27 08:29:30 +0800

    Changed designs in profile template.

--- TangoWithDjango/static/js/profile.js
+++ TangoWithDjango/static/js/profile.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-    $('#edit-profile-btn').click(function (e) { 
-        // e.preventDefault();
-        // Do something here
-    });
+    // $('#edit-profile-btn').click(function (e) { 
+    //     e.preventDefault();
+    //     console.log('test');
+        
+    //     $(this).addClass('hide');
+    //     $('#save-changes-btn').removeClass('hide');
+    // });
+
+    // $('#save-changes-btn').click(function (e) { 
+    //     // e.preventDefault();
+    //     $(this).addClass('hide');
+    //     $('#edit-profile-btn').removeClass('hide');
+    // });
 });
